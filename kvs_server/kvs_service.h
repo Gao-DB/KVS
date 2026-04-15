927d782f01a9b193b3af36b2fc7f02c6d0c875f6
Gao-DB <gao413616@gmail.com>
2026-04-15 15:08:53 +0800

    Refactor kvs_service.h to include stream and encode details

--- kvs_server/kvs_service.h
+++ kvs_server/kvs_service.h
@@ -9,6 +9,29 @@
 #define KVS_SERVICE_STATUS_OK    (0)
 #define KVS_SERVICE_STATUS_ERR   (-1)
 
+typedef struct
+{
+    char m_StreamName[COMMON_STRING_LEN];
+    char m_EncodeType[COMMON_STRING_LEN];
+    int  m_Enable;
+    int  m_Resolution;  //0 :for D1(960*540), 1:720P(1280*720) 2:1080P(1920*1080) 3:300w(2304*1296) 4:400w:2K(2560*1440) 5:4K(3840*2160)
+    int  m_FPS;
+    int  m_GOP;
+    int  m_mBpsMode;    //0: VBR; 1:CBR mode;
+    int  m_mBps;	
+}VencCfg;
+// 4.7 Audio encode function config
+typedef struct
+{
+    int m_Enable;
+    int m_Type;             //0:G711A 1:G711U 2:G722 3:G726
+    int m_SampleRate;		//in:8000;11025;12000;16000;22050;24000;32000;44100;48000;64000;96000;
+    int m_bit_Width;		//sample bit width : 8,16,24;
+    int m_Channel_Mode;     //0:MONO 1:STEREO
+    int m_Volume;			// [0,100]
+    int m_audio_local_enable;  // sound location
+}AencCfg;
+
  //kvs service config  
 typedef struct
 {
